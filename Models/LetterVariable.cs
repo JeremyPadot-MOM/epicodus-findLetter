c68abb3e81836648c2461f428708c5754b157427
Student <example@example.com>
2020-03-09 09:18:32 -0700

    add Location and Souvenirs fields.

--- Models/LetterVariable.cs
+++ Models/LetterVariable.cs
@@ -4,6 +4,7 @@ namespace FriendLetter.Models
   {
     public string Recipient { get; set; }
     public string Sender { get; set; }
-    
+    public string Location { get; set; }
+    public string Souvenirs { get; set; }
   }
 }
